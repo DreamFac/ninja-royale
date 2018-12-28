3f32aa9a9c023ab444769d2bc9bfd80f818d33a5
Oscar Diaz <odiaz.dw@gmail.com>
2018-12-28 02:50:46 -0600

    Adding Ninja Character
    Main camera script position/rotation.

--- Ninja Royale/Assets/Project/Scripts/MainCamera.cs
+++ Ninja Royale/Assets/Project/Scripts/MainCamera.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCamera : MonoBehaviour {
+public class MainCamera : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = new Vector3(0f, 2f, -2f);
+        transform.rotation = Quaternion.Euler(25f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ninja-royale
 ninja royla game with unity
+
+# Download Warrior Assets
+- https://assetstore.unity.com/packages/3d/animations/warrior-pack-bundle-1-free-36405
