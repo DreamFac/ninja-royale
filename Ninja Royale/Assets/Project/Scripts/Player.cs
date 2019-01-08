c6f2fe56ea1f280fb25dfc220e175f6e7a695b00
Eduardo Aviles <eduardo.avilesj@gmail.com>
2019-01-07 21:24:02 -0600

    adding camera and jump behavior

---
+++ Ninja Royale/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour {
+    [Header("Focal point variables")]
+    [SerializeField] private GameObject focalPoint;
+    [SerializeField] private float focalDistance;
+    [SerializeField] private float focalSmoothness;
+    [SerializeField] public KeyCode changeFocalSideKey;
+    public bool isFocalPointOnLeft = false;
+    // Use this for initialization
+
+    Animator m_Animator;
+
+
+    void Start () {
+        m_Animator = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(changeFocalSideKey))
+        {
+            isFocalPointOnLeft = !isFocalPointOnLeft;
+            m_Animator.SetBool("SwitchSide", isFocalPointOnLeft);
+        }
+        float targetX = focalDistance * (isFocalPointOnLeft ? -1 : 1);
+        float smoothX = Mathf.Lerp(focalPoint.transform.localPosition.x, targetX, focalSmoothness * Time.deltaTime);
+        focalPoint.transform.localPosition = new Vector3(smoothX, focalPoint.transform.localPosition.y, focalPoint.transform.localPosition.z);
+    }
+
+
+
+}
